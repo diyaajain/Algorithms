d28b0964294527bf59e2af1535243ab9e3ef33da
yukaymmt <yamamtyuka@gmail.com>
2024-03-27 17:50:23 -0400

    read files

--- project2.cpp
+++ project2.cpp
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+    FILE *file;
+    int k, m, n;
+    val sequence;  //have to redefine
+    //define matrix
+
+    //open file for reading
+    file = fopen("input.txt", "r");
+    
+    fscanf(file, "%d %d %d", &k, &m, &n);
+
+    //read the sequence
+    for (int i = 0; i < k; i++){
+        fscanf(file, "%d", sequence[i]);
+    }
+
+    //read the matrx
+    for (int i = 0;  i < m; i++){
+        for (int j = 0; j < n; j++){
+            fscanf(file, "%d", matrix[i][j]);
+        }
+    }
+    
+    fclose(file);
+    return 0;
+}
